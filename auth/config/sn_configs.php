38cc9f382ca312f3adf20d16570b8243589a4759
Martin Meltsas <martin.meltsas@khk.ee>
2016-07-05 01:30:52 +0300

    fixed sample config

--- auth/config/sn_configs.php
+++ auth/config/sn_configs.php
@@ -1,24 +1,34 @@
 <?php
 define('CONFIG',
     [
-        "base_url"   => "http://snauth.esy.es/",
-        "providers"  => [
-            "google"   => [
+        "base_url"   => "https://net.era.ee/auth/", //redirect URI
+        "providers" => [
+            "google" => [
                 "enabled" => true,
-                "keys"    => [ "id" => "", "secret" => "" ],
+                "keys" => ["id" => "YOUR_APP_ID", "secret" => "YOUR_APP_SECRET"],
+                "permissions" => ['email', 'profile']
             ],
             "facebook" => [
-                "enabled"        => true,
-                "keys"           => [ "id" => "", "secret" => "" ],
+                "enabled" => true,
+                "keys" => ["id" => "YOUR_APP_ID", "secret" => "YOUR_APP_SECRET", "version" => 'v2.6'],
+                "permissions" => ['email', 'public_profile', 'user_friends'],
                 "trustForwarded" => false
             ],
-            "twitter"  => [
+            "twitter" => [
                 "enabled" => true,
-                "keys"    => [ "key" => "", "secret" => "" ]
+                "keys" => ["id" => "YOUR_APP_ID", "secret" => "YOUR_APP_SECRET"],
+                "permissions" => ['r_basicprofile', 'r_emailaddress']
+            ],
+            "linkedin" => [
+                "enabled" => true,
+                "keys" => ["id" => "YOUR_APP_ID", "secret" => "YOUR_APP_SECRET"],
+                "permissions" => ['r_basicprofile', 'r_emailaddress']
+            ],
+            "github" => [
+                "enabled" => true,
+                "keys" => ["id" => "YOUR_APP_ID", "secret" => "YOUR_APP_SECRET"],
+                "permissions" => ['user', 'user:email', 'repo']
             ],
         ],
-        "debug_mode" => true,
-        "debug_file" => "bug.txt",
     ]
-
 );
